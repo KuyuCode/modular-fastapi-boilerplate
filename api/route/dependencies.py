f53794cdd1ce7e3fd8fe8df87406bc9a82eb864a
Kuyugama <mail.kuyugama@gmail.com>
2025-05-31 01:19:30 +0300

    Fix import issue

--- api/route/dependencies.py
+++ api/route/dependencies.py
@@ -1,11 +1,10 @@
-from typing import AsyncGenerator
+from collections.abc import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.session_holder import session_holder
+from api.session_holder import session_holder
 
 
 async def acquire_session() -> AsyncGenerator[AsyncSession, None]:
     async with session_holder.session() as session:
         yield session
-
